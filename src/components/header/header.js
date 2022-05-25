9c9793b8cf3576b45553f8f29b330d787315073e
Sandra Jurberga-Šaudine <sandra.jurberga@gmail.com>
2022-05-25 20:11:37 +0300

    mobile menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,19 @@
 import "./header.css"
+import React, {useState} from "react"
 
 function Header(){
+const [open, setOpen] = useState(false)
+
+    const menu =()=>{
+setOpen(current => !current)
+    }
+
     return(
 <div className="header">
     <nav>
         <p>crowdfund</p>
-        <ul>
+        <div className={open ? "closemenu" : "hamburger"} onClick={menu}></div>
+        <ul className={open ? "" : "none"}>
             <li>About</li>
             <li>Discover</li>
             <li>Get Started</li>
